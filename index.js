f04869c83f61c8f77dc5e112ab1f9622088d824a
shai horev <shaiho31@gmail.com>
2021-06-23 22:10:14 +0300

    layouts and functionality

---
+++ index.js
@@ -0,0 +1,44 @@
+const box1 = document.getElementById("td-box1")
+box1.addEventListener("click", () => {
+    console.log("clicked on box1")
+})
+
+const box2 = document.getElementById("td-box2")
+box2.addEventListener("click", () => {
+    console.log("clicked on box2")
+})
+
+const box3 = document.getElementById("td-box3")
+box3.addEventListener("click", () => {
+    console.log("clicked on box3")
+})
+
+const box4 = document.getElementById("td-box4")
+box4.addEventListener("click", () => {
+    console.log("clicked on box4")
+})
+
+const box5 = document.getElementById("td-box5")
+box5.addEventListener("click", () => {
+    console.log("clicked on box5")
+})
+
+const box6 = document.getElementById("td-box6")
+box6.addEventListener("click", () => {
+    console.log("clicked on box6")
+})
+
+const box7 = document.getElementById("td-box7")
+box7.addEventListener("click", () => {
+    console.log("clicked on box7")
+})
+
+const box8 = document.getElementById("td-box8")
+box8.addEventListener("click", () => {
+    console.log("clicked on box8")
+})
+
+const box9 = document.getElementById("td-box9")
+box9.addEventListener("click", () => {
+    console.log("clicked on box9")
+})
